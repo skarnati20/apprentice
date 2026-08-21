0bd55a80dc88aaa0b52334d1f18f6c6ef5736cbf
Sai Karnati <saikarnati2002@gmail.com>
2026-08-20 22:44:36 -0400

    apprentice: add custom loop drop-ins

--- apprentice.lisp
+++ apprentice.lisp
@@ -7,11 +7,29 @@
 (defparameter *allowed-dirs* nil)
 
 
-(defun chat (prompt)
-  (destructuring-bind (content msgs)
-      (little-coder-loop prompt :history *chat-history*)
-    (setf *chat-history* msgs)
-    (format t "~a" content)))
+(defun resolve-loop (loop-symbol)
+  "Returns the appropriate loop function symbol or nil if unknown."
+  (ecase loop-symbol
+    (:little-coder 'little-coder-loop)
+    (:standard 'standard-loop)
+    (:default 'standard-loop)
+    (t nil)))
+
+(defun chat (prompt &optional loop-symbol)
+  "CHAT runs a conversation using the specified loop.
+   
+  LOOP-SYMBOL can be one of:
+    :little-coder - use little-coder-loop
+    :standard - use standard-loop
+    :default - use standard-loop (default)
+    nil or t - use standard-loop (default)"
+  (let ((loop-fn (resolve-loop loop-symbol)))
+    (if loop-fn
+        (destructuring-bind (content msgs)
+            (funcall loop-fn prompt)
+          (setf *chat-history* msgs)
+          (format t "~a" content))
+      (error "Unknown loop: ~a" loop-symbol))))
 
 (defun add-allowed-dir (dir)
   (push dir *allowed-dirs*))
