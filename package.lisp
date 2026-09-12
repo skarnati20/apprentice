ac80e511ba71edffe2a608a42868706912ad38b0
Sai Karnati <saikarnati2002@gmail.com>
2026-09-11 22:54:42 -0400

    package: add main apprentice functions

--- package.lisp
+++ package.lisp
@@ -6,14 +6,26 @@
   (:export ;; Conversation
 	   #:chat
 	   #:clear
+	   #:drop-turns
+	   #:show-turns
 	   #:resolve-loop
 	   ;; Models
 	   #:models
 	   #:curr-model
 	   #:set-model
+	   ;; Anchors
+	   #:anchors
+	   #:available-anchors
+	   #:add-anchor
+	   #:clear-anchors
+	   #:set-anchor-dir
 	   ;; Permissions
 	   #:add-allowed-dir
 	   #:clear-allowed-dirs
 	   ;; Loops
 	   #:curr-loop
-	   #:set-loop))
+	   #:set-loop
+	   ;; Options
+	   #:options
+	   #:add-option
+	   #:clear-options))
